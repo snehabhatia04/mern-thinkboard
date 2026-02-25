069079b9f3a77b419006ea4c4edc02bf0e4e65b9
Snehabhatia04 <snehabhatia2004@gmail.com>
2026-02-25 18:02:58 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
@@ -11,15 +12,25 @@ console.log(process.env.MONGO_URL);
 
 const app=express()
 const PORT=process.env.PORT || 5001;
+const __dirname= path.resolve();
 
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }));
+}
 
-app.use(cors({
-    origin: "http://localhost:5173",
-}));
 app.use(express.json()); //middleware to parse JSON bodies
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
